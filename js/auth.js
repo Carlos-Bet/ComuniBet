ea74f7318d40639684ca5ab7868d0828ee27e373
Carlos-Bet <carlosmontenegroc27@gmail.com>
2026-03-06 00:50:08 -0500

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,27 +1,28 @@
-import { auth } from "./firebase.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-import { GoogleAuthProvider, signInWithPopup, signOut }
+const firebaseConfig = {
+  apiKey: "AIzaSyAs19oiYivfPYSCt_-xcOFeBQMziZSeBno",
+  authDomain: "comunibetstudio.firebaseapp.com",
+  projectId: "comunibetstudio",
+  storageBucket: "comunibetstudio.firebasestorage.app",
+  messagingSenderId: "980411984792",
+  appId: "1:980411984792:web:c7ab103a9efe8e121e99ed",
+  measurementId: "G-P6EJLZRTVV"
+};
 
-from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
-
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
-window.loginGoogle = async function(){
-
-const result = await signInWithPopup(auth,provider);
-
-if(result.user){
-
-location.href="foro.html";
-
-}
-
-}
-
-window.logout = async function(){
-
-await signOut(auth);
-
-location.href="index.html";
-
-}
+document.getElementById("loginGoogle").addEventListener("click", () => {
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      const user = result.user;
+      console.log("Usuario:", user);
+      alert("Sesión iniciada como " + user.displayName);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
